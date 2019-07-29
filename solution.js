4e7e35e9fd59b94d86078a6dce350096f2b0c36a
ytsuji27 <ytsuji91@gmail.com>
2019-07-29 12:32:02 -0700

    Passed all tests

--- solution.js
+++ solution.js
@@ -1,5 +1,9 @@
 const peakIndexInMountainArray = arr => {
-
+  let i = 0;
+  while (arr[i] < arr[i+1]) {
+    i++;
+  }
+  return i;
 }
 
 module.exports = { peakIndexInMountainArray }
--- test/test.js
+++ test/test.js
@@ -7,5 +7,5 @@ it('successfully finds the peak of [0,1,0]', () => {
 })
 
 it('successfully finds the peak of [0,2,1,0]', () => {
-  assert.equal(solution.peakIndexInMountainArray([0,1,2,0]),1);
+  assert.equal(solution.peakIndexInMountainArray([0,2,1,0]),1);
 })
